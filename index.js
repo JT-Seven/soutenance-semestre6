9cf23ccc3955a5010d34d6b09fe0c4488b3f617e
Steven <terneus@et.intechinfo.fr>
2023-07-06 15:16:29 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+export default function getCustomBoxStyles() {
+    return `
+        height: 1000px;
+        backdrop-filter: blur(20px);
+        background: #0000002b;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    `;
+}
